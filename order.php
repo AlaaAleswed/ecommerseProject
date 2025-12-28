9ffbdf9ca9ec8e0c6f82d11ab60b93e7e71ae0c0
AlaaAleswed <alaahanialeswed@gmail.com>
2025-12-28 20:24:40 +0300

    fixing the header

--- order.php
+++ order.php
@@ -1,4 +1,5 @@
 <?php include "includes/header.php"; ?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
 <div class="container my-5">
     <div class="row justify-content-center">
@@ -53,7 +54,7 @@
                                 </div>
                             </div>
                         </div>
-                        
+
                         <hr>
 
                         <div class="mb-3">
@@ -87,5 +88,6 @@
         </div>
     </div>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 <?php include "includes/footer.php"; ?>
